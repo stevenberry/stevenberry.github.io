dae610f5fbf7273dbd4b469b9d964a289c1354a8
Steven Berry <steve.berry@yourbigears.com>
2018-08-01 20:39:21 +0100

    Website

---
+++ assets/js/bg.js
@@ -0,0 +1,26 @@
+(() => {
+
+    if (screen.width > 650) {
+        var londonbtn = document.getElementById("londonbtn")
+        var video1 = document.getElementById('bg-vid1');
+        var video2 = document.getElementById('bg-vid2');
+        var video3 = document.getElementById('bg-vid3');
+
+        video1.innerHTML = '<source id="bg-vid1-src" src="images/bg1.mp4" type="video/mp4">'
+        video2.innerHTML = '<source id="bg-vid2-src" src="images/bg2.mp4" type="video/mp4">'
+        video3.innerHTML = '<source id="bg-vid3-src" src="images/bg3.mp4" type="video/mp4">'
+
+        video1.onended = () => {
+            video2.play()
+            video2.classList.toggle('hidden')
+        };
+        londonbtn.onclick = (e) => {
+            e.preventDefault()
+            video3.play()
+            video3.classList.toggle('hidden')
+            video1.classList.toggle('hidden')
+            video2.classList.toggle('hidden')
+        };
+    }
+
+})();
