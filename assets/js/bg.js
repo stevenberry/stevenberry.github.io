518bda5794b2273eb3d0f1e0df0e917f4c2716c3
Steven Berry <steve.berry@yourbigears.com>
2018-08-02 17:30:51 +0100

    Elliot server

--- assets/js/bg.js
+++ assets/js/bg.js
@@ -5,8 +5,8 @@
         var video1 = document.getElementById('bg-vid1');
         var video2 = document.getElementById('bg-vid2');
 
-        video1.innerHTML = '<source id="bg-vid1-src" src="images/bg1.mp4" type="video/mp4">'
-        video2.innerHTML = '<source id="bg-vid2-src" src="images/bg2.mp4" type="video/mp4">'
+        video1.innerHTML = '<source id="bg-vid1-src" src="https://cdn.elken.io/site/bg/bg1.mp4" type="video/mp4">'
+        video2.innerHTML = '<source id="bg-vid2-src" src="https://cdn.elken.io/site/bg/bg2.mp4" type="video/mp4">'
 
         video1.onended = () => {
             video2.play()
@@ -19,14 +19,14 @@
             video1.classList.toggle('hidden')
             video2.classList.toggle('hidden')
         };
-        } else {
-        var background = document.getElementById('bg-mobile');
-        background.innerHTML = '<img id="bg-mobile-pic" class="fullscreen-bg-pic" src="https://cdn.elken.io/site/bg/bg-mobile.jpg">'
-    }    
-    
-            var video3 = document.getElementById('bg-vid3');
-            video3.innerHTML = '<source id="bg-vid3-src" src="images/bg3.mp4" type="video/mp4">'
-
+//        } else {
+//        var background = document.getElementById('bg-mobile');
+//        background.innerHTML = '<img id="bg-mobile-pic" class="fullscreen-bg-pic" src="https://cdn.elken.io/site/bg/bg-mobile.jpg">'
+//    }    
+//    
+//            var video3 = document.getElementById('bg-vid3');
+//            video3.innerHTML = '<source id="bg-vid3-src" src="images/bg3.mp4" type="video/mp4">'
+//
 
 
 })();
