f2826e1ebe9e98f74addd59f1c4527dfd894d569
Steven Berry <steve.berry@yourbigears.com>
2018-08-02 19:09:10 +0100

    Updates five

--- assets/js/bg.js
+++ assets/js/bg.js
@@ -5,8 +5,8 @@
         var video1 = document.getElementById('bg-vid1');
         var video2 = document.getElementById('bg-vid2');
 
-        video1.innerHTML = '<source id="bg-vid1-src" src="https://cdn.elken.io/site/bg/bg1.mp4" type="video/mp4">'
-        video2.innerHTML = '<source id="bg-vid2-src" src="https://cdn.elken.io/site/bg/bg2.mp4" type="video/mp4">'
+        video1.innerHTML = '<source id="bg-vid1-src" src="https://cdn.elken.io/site/img/bg/bg1.mp4" type="video/mp4">'
+        video2.innerHTML = '<source id="bg-vid2-src" src="https://cdn.elken.io/site/img/bg/bg2.mp4" type="video/mp4">'
 
         video1.onended = () => {
             video2.play()
@@ -21,11 +21,11 @@
         };
         } else {
         var background = document.getElementById('bg-mobile');
-        background.innerHTML = '<img id="bg-mobile-pic" class="fullscreen-bg-pic" src="https://cdn.elken.io/site/bg/bg-mobile.jpg">'
+        background.innerHTML = '<img id="bg-mobile-pic" class="fullscreen-bg-pic" src="https://cdn.elken.io/site/img/bg/bg-mobile.jpg">'
     }    
     
             var video3 = document.getElementById('bg-vid3');
-            video3.innerHTML = '<source id="bg-vid3-src" src="https://cdn.elken.io/questionconsulting/bg/bg3.mp4">'
+            video3.innerHTML = '<source id="bg-vid3-src" src="https://cdn.elken.io/questionconsulting/img/bg/bg3.mp4">'
 
 
 
